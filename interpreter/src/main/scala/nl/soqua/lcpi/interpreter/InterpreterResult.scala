a9658fe55bcfe2ac2da4f0872f4e2393fd203868
Kevin van der Vlist <kvdvlist@sogyo.nl>
2017-02-20 19:26:15 +0100

    Add a toggle for an ascii mode
    This is disabled by default. If enabled it will render λ characters as \\
    so that they can be displayed on terminals that don't support these characters.

--- interpreter/src/main/scala/nl/soqua/lcpi/interpreter/InterpreterResult.scala
+++ interpreter/src/main/scala/nl/soqua/lcpi/interpreter/InterpreterResult.scala
@@ -11,8 +11,13 @@ object InterpreterResult {
 sealed trait InterpreterResult {
   val expression: Expression
   val context: Context
+  def map(f: Expression => Expression): InterpreterResult
 }
 
-case class SingleExpressionInterpreterResult(context: Context, expression: Expression) extends InterpreterResult
+case class SingleExpressionInterpreterResult(context: Context, expression: Expression) extends InterpreterResult {
+  override def map(f: (Expression) => Expression): InterpreterResult = copy(expression = f(expression))
+}
 
-case class TraceInterpreterResult(context: Context, expression: Expression, trace: List[(String, Expression)]) extends InterpreterResult
+case class TraceInterpreterResult(context: Context, expression: Expression, trace: List[(String, Expression)]) extends InterpreterResult {
+  override def map(f: (Expression) => Expression): InterpreterResult = copy(expression = f(expression))
+}
--- repl/src/main/scala/nl/soqua/lcpi/repl/monad/ReplState.scala
+++ repl/src/main/scala/nl/soqua/lcpi/repl/monad/ReplState.scala
@@ -5,12 +5,16 @@ import nl.soqua.lcpi.repl.lib.CombinatorLibrary
 
 private[monad] sealed trait TraceModePosition
 
-private[monad] case object Enabled extends TraceModePosition
+private[monad] sealed trait AsciiModeToggle
 
-private[monad] case object Disabled extends TraceModePosition
+private[monad] case object Enabled extends TraceModePosition with AsciiModeToggle
+
+private[monad] case object Disabled extends TraceModePosition with AsciiModeToggle
 
 object ReplState {
-  val empty: ReplState = ReplState(CombinatorLibrary.loadIn(Context()), traceMode = Disabled, terminated = false, contextIsMutable = false, List.empty)
+  val empty: ReplState = ReplState(CombinatorLibrary.loadIn(Context()), traceMode = Disabled, asciiMode = Disabled,
+    terminated = false, contextIsMutable = false, List.empty)
 }
 
-case class ReplState(context: Context, traceMode: TraceModePosition, terminated: Boolean, contextIsMutable: Boolean, reloadableFiles: List[String])
+case class ReplState(context: Context, traceMode: TraceModePosition, asciiMode: AsciiModeToggle, terminated: Boolean,
+                     contextIsMutable: Boolean, reloadableFiles: List[String])
